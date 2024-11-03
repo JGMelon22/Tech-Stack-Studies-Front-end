68343ab8b43a6e69caaf428cfa89071dd7c66dbf
JGMelon22 <jgvasconcellos22@gmail.com>
2024-11-03 10:39:17 -0300

    feat: include routes and webHistory to main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap'
+import { createRoute, createWebHistory } from 'vue-router'
+import TechStackList from './components/pages/TechStackList'
+import TechStackCreate from './components/pages/TechStackCreate'
+import TechStackEdit from './components/pages/TechStackEdit'
+import TechStackShow from './components/pages/TechStackShow'
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+
+const router = createRoute({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: TechStackList },
+        { path: '/create', component: TechStackCreate },
+        { path: '/edit/:id', component: TechStackEdit },
+        { path: '/show/:id', component: TechStackShow }
+    ]
+});
+
+createApp(App).use(router).mount('#app')
